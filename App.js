4dd1acf8c6df24f224ebe1ff4188a7aeedd09d63
Moamin <moamin.salamah@gmail.com>
2023-12-24 09:23:03 +0100

    project setup

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
+
+export default function App() {
+  return (
+    <View>
+      <AppNavigator />
+      <StatusBar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
