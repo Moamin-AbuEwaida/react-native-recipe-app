1c09fab99431e325b064d95526e17e2f557b3e74
Moamin <moamin.salamah@gmail.com>
2023-12-24 10:11:23 +0100

    search component

--- src/screens/RecipeScreen.js
+++ src/screens/RecipeScreen.js
@@ -1,11 +1,13 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Header from "../components/Header";
+import SearchFilter from "../components/SearchFilter";
 
 const RecipeScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1, marginHorizontal: 16 }}>
       <Header headerText={"Hello, Dude 😃"} headerIcon={"bell-o"} />
+      <SearchFilter icon="search" placeholder={"enter your fav. recipe "} />
     </SafeAreaView>
   );
 };
